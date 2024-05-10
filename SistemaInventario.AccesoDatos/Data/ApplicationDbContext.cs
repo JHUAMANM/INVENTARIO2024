f2754e847e9db01565873e03973fe834efa7d899
JHUAMANM <joelhm3101@gmail.com>
2024-05-09 21:22:43 -0500

    Proyecto

---
+++ SistemaInventario.AccesoDatos/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SistemaInventario.Modelos;
+using System.Reflection;
+
+namespace SistemaInventario.AccesoDatos.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        //Agregar todos los modelos creados 
+        public DbSet<Bodega> Bodegas { get; set; }
+
+
+
+
+
+        //Creamos un override para un metodo que ya existe en dbContext
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
